dc3fa3453c657198411b05cccf0789e2a60521d5
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-12-16 18:59:17 +0530

    registrations modified

--- src/pages/Authentication/Register/Registration.jsx
+++ src/pages/Authentication/Register/Registration.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import "./Registration.css";
-import RegisterSection from '../../components/RegisterSections/RegisterSection'
+import RegisterSection from '../../../components/RegisterSections/RegisterSection'
 
 const Registration = () => {
 
