cc8ab6c676a815767fa93fb16513ce825516534a
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-12-17 02:11:55 +0530

    register progress style update

--- src/components/RegisterSections/RegisterSection.jsx
+++ src/components/RegisterSections/RegisterSection.jsx
@@ -5,9 +5,9 @@ const RegisterSection = ({firstSectionInfo, secondSectionInfo, thirdSectionInfo}
 
     return (
         <div className="section__progress">
-            <span className={`progress__bar ${firstSectionInfo && 'make__progress'}`}></span>
-            <span className={`progress__bar ${secondSectionInfo && 'make__progress'}`}></span>
-            <span className={`progress__bar ${thirdSectionInfo && 'make__progress'}`}></span>
+            <span className={`register__progress ${firstSectionInfo && 'make__progress'}`}></span>
+            <span className={`register__progress ${secondSectionInfo && 'make__progress'}`}></span>
+            <span className={`register__progress ${thirdSectionInfo && 'make__progress'}`}></span>
         </div>
     )
 }
